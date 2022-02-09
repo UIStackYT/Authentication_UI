40e6e9b9c7c6d62ffbf652a9bcabedba0ec7286a
20akumbhar <20akumbhar@gmail.com>
2022-02-09 20:54:27 +0530

    Settings Screen

--- app/src/main/java/com/uistack/jetpackcompose4/MainActivity.kt
+++ app/src/main/java/com/uistack/jetpackcompose4/MainActivity.kt
@@ -4,16 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
 import com.uistack.jetpackcompose4.ui.screens.ForgotPasswordScreen
 import com.uistack.jetpackcompose4.ui.screens.LoginScreen
 import com.uistack.jetpackcompose4.ui.screens.RegisterScreen
+import com.uistack.jetpackcompose4.ui.screens.SettingsScreen
 import com.uistack.jetpackcompose4.ui.theme.BackgroundColor
 import com.uistack.jetpackcompose4.ui.theme.PrimaryColor
 import com.uistack.jetpackcompose4.ui.theme.JetpackCompose4Theme
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalMaterialApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -21,7 +24,8 @@ class MainActivity : ComponentActivity() {
                 Surface(color = BackgroundColor, modifier = Modifier.fillMaxSize()) {
 //LoginScreen()
 //                    RegisterScreen()
-                    ForgotPasswordScreen()
+//                    ForgotPasswordScreen()
+                    SettingsScreen()
                 }
             }
         }
---
+++ app/src/main/java/com/uistack/jetpackcompose4/ui/screens/SettingsScreen.kt
@@ -0,0 +1,297 @@
+package com.uistack.jetpackcompose4.ui.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.uistack.jetpackcompose4.R
+import com.uistack.jetpackcompose4.ui.theme.*
+
+@ExperimentalMaterialApi
+@Composable
+fun SettingsScreen() {
+    Column() {
+        HeaderText()
+        ProfileCardUI()
+        GeneralOptionsUI()
+        SupportOptionsUI()
+    }
+}
+
+
+@Composable
+fun HeaderText() {
+    Text(
+        text = "Settings",
+        fontFamily = Poppins,
+        color = SecondaryColor,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 30.dp, bottom = 10.dp),
+        fontWeight = FontWeight.ExtraBold,
+        fontSize = 16.sp
+    )
+}
+
+@Composable
+fun ProfileCardUI() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .padding(10.dp),
+        backgroundColor = Color.White,
+        elevation = 0.dp,
+        shape = Shapes.large
+    ) {
+        Row(
+            modifier = Modifier.padding(20.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column() {
+                Text(
+                    text = "Check Your Profile",
+                    fontFamily = Poppins,
+                    color = SecondaryColor,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                )
+
+                Text(
+                    text = "UI.Stack.YT@gmail.com",
+                    fontFamily = Poppins,
+                    color = Color.Gray,
+                    fontSize = 10.sp,
+                    fontWeight = FontWeight.SemiBold,
+                )
+
+                Button(
+                    modifier = Modifier.padding(top = 10.dp),
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = PrimaryColor
+                    ),
+                    contentPadding = PaddingValues(horizontal = 30.dp),
+                    elevation = ButtonDefaults.elevation(
+                        defaultElevation = 0.dp,
+                        pressedElevation = 2.dp
+                    ),
+                    shape = Shapes.medium
+                ) {
+                    Text(
+                        text = "View",
+                        fontFamily = Poppins,
+                        color = SecondaryColor,
+                        fontSize = 12.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+            Image(
+                painter = painterResource(id = R.drawable.ic_profile_card_image),
+                contentDescription = "",
+                modifier = Modifier.height(120.dp)
+            )
+        }
+    }
+}
+
+@ExperimentalMaterialApi
+@Composable
+fun GeneralOptionsUI() {
+    Column(
+        modifier = Modifier
+            .padding(horizontal = 14.dp)
+            .padding(top = 10.dp)
+    ) {
+        Text(
+            text = "General",
+            fontFamily = Poppins,
+            color = SecondaryColor,
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(vertical = 8.dp)
+        )
+        GeneralSettingItem(
+            icon = R.drawable.ic_rounded_notification,
+            mainText = "Notifications",
+            subText = "Customize notifications",
+            onClick = {}
+        )
+        GeneralSettingItem(
+            icon = R.drawable.ic_more,
+            mainText = "More customization",
+            subText = "Customize it more to fit your usage",
+            onClick = {}
+        )
+//        GeneralSettingItem()
+    }
+}
+
+@ExperimentalMaterialApi
+@Composable
+fun GeneralSettingItem(icon: Int, mainText: String, subText: String, onClick: () -> Unit) {
+    Card(
+        onClick = { onClick() },
+        backgroundColor = Color.White,
+        modifier = Modifier
+            .padding(bottom = 8.dp)
+            .fillMaxWidth(),
+        elevation = 0.dp,
+    ) {
+        Row(
+            modifier = Modifier.padding(vertical = 10.dp, horizontal = 14.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Box(
+                    modifier = Modifier
+                        .size(34.dp)
+                        .clip(shape = Shapes.medium)
+                        .background(LightPrimaryColor)
+                ) {
+                    Icon(
+                        painter = painterResource(id = icon),
+                        contentDescription = "",
+                        tint = Color.Unspecified,
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+
+                Spacer(modifier = Modifier.width(14.dp))
+                Column(
+                    modifier = Modifier.offset(y = (2).dp)
+                ) {
+                    Text(
+                        text = mainText,
+                        fontFamily = Poppins,
+                        color = SecondaryColor,
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Bold,
+                    )
+
+                    Text(
+                        text = subText,
+                        fontFamily = Poppins,
+                        color = Color.Gray,
+                        fontSize = 10.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        modifier = Modifier.offset(y = (-4).dp)
+                    )
+                }
+            }
+            Icon(
+                painter = painterResource(id = R.drawable.ic_right_arrow),
+                contentDescription = "",
+                modifier = Modifier.size(16.dp)
+            )
+
+        }
+    }
+}
+
+@ExperimentalMaterialApi
+@Composable
+fun SupportOptionsUI() {
+    Column(
+        modifier = Modifier
+            .padding(horizontal = 14.dp)
+            .padding(top = 10.dp)
+    ) {
+        Text(
+            text = "Support",
+            fontFamily = Poppins,
+            color = SecondaryColor,
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(vertical = 8.dp)
+        )
+        SupportItem(
+            icon = R.drawable.ic_whatsapp,
+            mainText = "Contact",
+            onClick = {}
+        )
+        SupportItem(
+            icon = R.drawable.ic_feedback,
+            mainText = "Feedback",
+            onClick = {}
+        )
+        SupportItem(
+            icon = R.drawable.ic_privacy_policy,
+            mainText = "Privacy Policy",
+            onClick = {}
+        )
+        SupportItem(
+            icon = R.drawable.ic_about,
+            mainText = "About",
+            onClick = {}
+        )
+    }
+}
+
+@ExperimentalMaterialApi
+@Composable
+fun SupportItem(icon: Int, mainText: String, onClick: () -> Unit) {
+    Card(
+        onClick = { onClick() },
+        backgroundColor = Color.White,
+        modifier = Modifier
+            .padding(bottom = 8.dp)
+            .fillMaxWidth(),
+        elevation = 0.dp,
+    ) {
+        Row(
+            modifier = Modifier.padding(vertical = 10.dp, horizontal = 14.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(34.dp)
+                        .clip(shape = Shapes.medium)
+                        .background(LightPrimaryColor)
+                ) {
+                    Icon(
+                        painter = painterResource(id = icon),
+                        contentDescription = "",
+                        tint = Color.Unspecified,
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+
+                Spacer(modifier = Modifier.width(14.dp))
+
+                    Text(
+                        text = mainText,
+                        fontFamily = Poppins,
+                        color = SecondaryColor,
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Bold,
+                    )
+            }
+            Icon(
+                painter = painterResource(id = R.drawable.ic_right_arrow),
+                contentDescription = "",
+                modifier = Modifier.size(16.dp)
+            )
+
+        }
+    }
+}
